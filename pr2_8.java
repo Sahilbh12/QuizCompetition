6ca76ec0a3efb369e2d526bbc61b4aeaac74ea29
Sahilbh12 <101415668+Sahilbh12@users.noreply.github.com>
2023-10-12 17:33:19 +0530

    Update pr2_8.java

--- pr2_8.java
+++ pr2_8.java
@@ -1056,7 +1056,7 @@ public class pr2_8 extends javax.swing.JFrame {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Welcome");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/java?user=root&password=$ahil2003");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/java?user=root&password=********");
             System.out.println("Connection created!");
             
             String s = "insert into score(name, points) values(?,?)";
